650294b38e6225bf7f0892e51a4346fb624e34d1
navins7 <48444400+navins7@users.noreply.github.com>
2019-03-14 09:33:52 +0530

    Updated design and layout

---
+++ website/insert.php
@@ -0,0 +1,41 @@
+<? php
+$Name = $_POST['Name'];
+$Location = $_POST['Location'];
+$Email = $_POST['Email'];
+$Complain = $_POST['message'];
+
+$host= "localhost";
+$dbUsername= "root";
+$dbPassword= "password";
+$dbname="orms";
+
+$conn= new mysqli($host,$dbName,$dbLocation,$dbEmail,$dbComplain);
+
+if (mysqli_connect_error()){
+	die('Connect Error('.mysqli_connect_error().')'.mysqli_connect_error());
+}else {
+	$SELECT= "SELECT Email From register where Email= ?Limit 1';
+	$INSERT= "INSERT Into register (Name,Location,Email,message) values(?,?,?,?)";
+
+	$stmt =$conn->prepare($SELECT);
+	$stmt->bind_param("s",$Email);
+	$stmt->execute();
+	$stmt->maxdb_bind_result($Email);
+	$stmt->store_result();
+	$rnum=$stmt->num_rows;
+
+	if ($rnum==0)
+	{
+		$stmt->close();
+
+		$stmt=$conn->prepare($INSERT);
+		$stmt->bind_param("hello",$Name,$Location,$Email,$message);
+		$stmt->execute();
+		echo "New record inserted";
+
+	}
+	$stmt->close();
+	$conn->close();
+}
+
+?>
